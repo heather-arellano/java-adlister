9c7ca1f001d304f56674f99ae434cf4e474dbce9
heather arellano <hma.485@icloud.com>
2018-06-04 14:15:41 -0500

    Completed MVC exercise

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -12,13 +12,13 @@ public class ViewProfileServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getMethod().equalsIgnoreCase("post")) {
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-            if (username.equals("admin") && password.equals("password")) {
-                request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (username.equals("admin") && password.equals("password")) {
+            request.getRequestDispatcher("/profile.jsp").forward(request, response);
 
-            }
+
         }
     }
+
 }
